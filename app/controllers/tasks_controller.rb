761344657b1f1159a6d87674a9156a742cee852f
JUAN <jsmith.jdhp@gmail.com>
2022-09-13 19:51:48 -0500

    casefinishRAILS

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
 
   def index
-    @task = Task.all
+    @tasks = Task.all
   end
 
   def show
@@ -9,6 +9,33 @@ class TasksController < ApplicationController
   end
 
   def new
-    @task = Task.create
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
